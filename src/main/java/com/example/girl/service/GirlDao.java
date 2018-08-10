320f84e660dc63ac55a7cfa6e99f1f3cec2a9eb8
renjk <1137497920@qq.com>
2018-08-10 16:50:01 +0800

    这个做动态查询的第一种方法

--- src/main/java/com/example/girl/service/GirlDao.java
+++ src/main/java/com/example/girl/service/GirlDao.java
@@ -18,20 +18,14 @@ public class GirlDao {
     EntityManager em;
 
     public List<Girl> getGirl(Integer age,String cm){
-
         CriteriaBuilder cb = em.getCriteriaBuilder();
-
         CriteriaQuery<Girl> query = cb.createQuery(Girl.class);
-
         //这里相当于SQL语句的from
         Root<Girl> root = query.from(Girl.class);
-
         //where
         Predicate p1 = null;
-
         if (age!=null){
             Predicate p2 = cb.equal(root.get(Girl_.age),age);   //放入条件
-
             if (p1 != null)     //如果不为空的话代表着这不是第一个参数
             {
                 p1 = cb.and(p1,p2);    //要加and
@@ -39,7 +33,6 @@ public class GirlDao {
                 p1 = p2;
             }
         }
-
         if (cm != null && !cm.equals("")){
             Predicate p2 = cb.equal(root.get(Girl_.cm),cm);
 
@@ -49,7 +42,6 @@ public class GirlDao {
                 p1 = p2;
             }
         }
-
         //把组合好的条件放到查询中
         query.where(p1);
 
